4f7ac355d21c28b8ae5581b42c954a2ae422029c
zhangxiaowei <zhangxiaowei@innobuddy.com>
2017-08-29 23:13:58 +0800

    修改部分eslint规则

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
 import path from 'path';
 import React from 'react';
+import express from 'express';
 import { renderToString } from 'react-dom/server';
 import StaticRouter from 'react-router-dom/StaticRouter';
 import { renderRoutes } from 'react-router-config';
 import routes from '../client/routes';
 
-const express = require('express');
-
+// 创建服务器实例
 const app = express();
-
-console.log('path', path.join(__dirname));
 
 // 设置视图引擎
 app.set('views', path.join(__dirname, '..', 'views'));
@@ -18,7 +16,7 @@ app.set('view engine', 'pug');
 // 设置静态资源服务器
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// 路由
+// 路由设置
 app.get('*', (req, res) => {
   const context = {};
   const content = renderToString(
@@ -27,16 +25,13 @@ app.get('*', (req, res) => {
     </StaticRouter>,
   );
 
+  // 服务端渲染
   res.render('index', {
     title: 'typee',
     content,
   });
 });
 
-const server = app.listen(3000, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+// 监听服务器3000端口
+app.listen(3000);
 

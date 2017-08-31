0be96266c16dc33deddc1953bf71bc5738b5784c
zhangxiaowei <zhangxiaowei@innobuddy.com>
2017-08-31 22:47:44 +0800

    添加样式文件并修改webpack配置打包分离出css样式

--- src/client/components/Header/index.jsx
+++ src/client/components/Header/index.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="component-header">
-    <ul>
-      <li><Link to="/">首页</Link></li>
-      <li><Link to="/lesson-list">课程</Link></li>
-      <li><Link to="/lesson-detail">课程详情页</Link></li>
-    </ul>
+    <div className="container">
+      <ul className="nav-list">
+        <li className="nav-item"><Link to="/">首页</Link></li>
+        <li className="nav-item"><Link to="/lesson-list">课程</Link></li>
+        <li className="nav-item"><Link to="/lesson-detail">课程详情页</Link></li>
+      </ul>
+    </div>
   </div>
 );
 
